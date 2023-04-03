b5eb270ddf5bdf202ded3e5aae2c4fe7d94db841
Upendra Kumar <toxic74412@gmail.com>
2023-04-03 22:37:15 +0530

    implemented unemplemented routes

--- controller/matchController.js
+++ controller/matchController.js
@@ -19,19 +19,67 @@ exports.getAllmatches = async (req, res) => {
 };
 
 exports.getUpcomingMatches = (req, res) => {
-  res.status(200).render("upcoming", {
-    title: "IPL Livescore | Get all ipl upcoming matches",
-  });
+  const matches = fs.readFileSync("matchdata/matchdata.json");
+  const response = JSON.parse(matches);
+  const allMatchList = response.data.matchList;
+  const upcomingMatchList = [];
+  for (let i = 0; i < allMatchList.length; i++) {
+    if (allMatchList[i].status === "Match not started")
+      upcomingMatchList.push(allMatchList[i]);
+  }
+  try {
+    res.status(200).render("upcoming", {
+      title: "IPL Livescore | Get all ipl upcoming matches",
+      data: upcomingMatchList,
+    });
+  } catch (error) {
+    res.status(404).json({
+      title: "Livescore | Something bad happen!",
+      error,
+    });
+  }
 };
 
 exports.getLiveMatches = (req, res) => {
-  res.status(200).render("live", {
-    title: "IPL Livescore | live Mathces",
-  });
+  const matches = fs.readFileSync("matchdata/matchdata.json");
+  const response = JSON.parse(matches);
+  const allMatchList = response.data.matchList;
+  const liveMatchList = [];
+  for (let i = 0; i < allMatchList.length; i++) {
+    if (allMatchList[i].status.includes("opt"))
+      liveMatchList.push(allMatchList[i]);
+  }
+  try {
+    res.status(200).render("upcoming", {
+      title: "IPL Livescore | Get all ipl live matches",
+      data: liveMatchList,
+    });
+  } catch (error) {
+    res.status(404).json({
+      title: "Livescore | Something bad happen!",
+      error,
+    });
+  }
 };
 
 exports.getFinishedMatches = (req, res) => {
-  res.status(200).render("finished", {
-    title: "IPL Livescore | Get all ipl upcoming matches",
-  });
+  const matches = fs.readFileSync("matchdata/matchdata.json");
+  const response = JSON.parse(matches);
+  const allMatchList = response.data.matchList;
+  const finishedMatchList = [];
+  for (let i = 0; i < allMatchList.length; i++) {
+    if (allMatchList[i].status.includes("won"))
+      finishedMatchList.push(allMatchList[i]);
+  }
+  try {
+    res.status(200).render("upcoming", {
+      title: "IPL Livescore | Get all ipl finished matches",
+      data: finishedMatchList,
+    });
+  } catch (error) {
+    res.status(404).json({
+      title: "Livescore | Something bad happen!",
+      error,
+    });
+  }
 };
